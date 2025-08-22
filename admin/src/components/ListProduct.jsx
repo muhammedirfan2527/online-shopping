9271aede43dc76f9a4e1dcd98a8883cca1c3c908
muhammedirfan2527 <167426270+muhammedirfan2527@users.noreply.github.com>
2025-08-23 03:41:37 +0530

    Update ListProduct.jsx

--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./List_Product.css";
+import "./ListProduct.css";
 import { Link } from "react-router-dom";
 
 const ListProduct = () => {
